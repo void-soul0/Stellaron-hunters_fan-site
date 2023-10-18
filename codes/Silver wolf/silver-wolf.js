0af32c533939f935328d6b29e997ddff93a634bc
void-soul0 <145861361+void-soul0@users.noreply.github.com>
2023-10-18 20:44:33 -0300

    Add files via upload

---
+++ codes/Silver wolf/silver-wolf.js
@@ -0,0 +1,11 @@
+confirm("voê deseja receber notificaçõees de produtos e serviços da CPI?")
+const botao= document.getElementById("logar"));
+botao.addEventListener("click",logar)
+function logar(){
+    return alert('login bem sucedido')
+}
+const botao2=document.getElementById("nlogar")
+botao2.addEventListener('click',naologar)
+function naologar(){
+    return confirm("tem certeza? ao fazer isso você perderá diversas promoções e beneficios exclusivos")
+}
